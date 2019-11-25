9ff3c8fd232f4c7bee86a2035a9259f462576f45
Daniel Chukwurah <dnchukwurah@sidmach.com>
2019-11-25 16:49:59 +0100

    feat: install date picker library

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Vuelidate from 'vuelidate'
 import axios from 'axios'
-import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+//import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
+//import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
 
 
 Vue.use(Vuelidate);
-Vue.use(VueCtkDateTimePicker);
+Vue.use(DatePicker)
+//Vue.use(VueCtkDateTimePicker);
 Vue.config.productionTip = false
 
 axios.defaults.headers.get['Accepts'] = 'application/json';
